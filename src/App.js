5db8cf939dd8f2216fbd0f65a4d4d969e199de2a
Rebecca <rebecca.bekele@masimo.com>
2025-04-16 11:33:04 -0500

    include login page on app.js

--- src/App.js
+++ src/App.js
@@ -5,21 +5,32 @@ import Navigation from './components/Navigation';
 import SidebarFilters from './components/SidebarFilters';
 import ProductGrid from './components/ProductGrid';
 import FeaturedCarousel from './components/FeaturedCarousel';
+import LoginForm from './components/Login/LoginForm';
 import './App.css'; 
 
 const App = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [selectedCategory, setSelectedCategory] = useState('');
+  const [showLogin, setShowLogin] = useState(false);
 
   return (
     <div>
       <Header />
       <Navigation onSearch={setSearchQuery} />
+      {!showLogin ? (
+        <>
       <FeaturedCarousel />
+    
       <div className="main-content">
         <SidebarFilters onFilterChange={setSelectedCategory} />
         <ProductGrid searchQuery={searchQuery} category={selectedCategory} />
       </div>
+      
+        <button onClick={() => setShowLogin(true)}>Login</button>
+        </>
+      ) : (
+        <LoginForm onLogin={() => {}} />
+      )}
     </div>
   );
 };
