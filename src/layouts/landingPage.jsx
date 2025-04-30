40bc45436c8de2f04e5d82749e1a495d019aefea
ademiltonnunes <ademilton.marcelo1@gmail.com>
2025-04-29 22:34:34 -0700

    adding HOC to landing page

--- src/layouts/landingPage.jsx
+++ src/layouts/landingPage.jsx
@@ -2,21 +2,21 @@ import React, { useState } from 'react';
 import HighlightedProductsCarousel from './HighlightedProductsCarousel';
 import SidebarFilters from './SidebarFilters';
 import ProductGrid from '../features/products/ProductGrid';
-import MainLayout from './MainLayout';
+import withLayout from '../HOC/withLayout';
 
 const LandingPage = () => {
     
   const [searchQuery, setSearchQuery] = useState('');
   const [selectedCategory, setSelectedCategory] = useState('');
   return (
-     <MainLayout>
+     <>
       <HighlightedProductsCarousel />
       <div className="main-content">
         <SidebarFilters onFilterChange={setSelectedCategory} />
         <ProductGrid searchQuery={searchQuery} category={selectedCategory} />
       </div>
-    </MainLayout>
+    </>
   );
 };
 
-export default LandingPage;
+export default withLayout(LandingPage);
