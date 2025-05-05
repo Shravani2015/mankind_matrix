7d1b0f090cdc4dbb237ee42b8bd8ab477396fdcf
Kaveri1496 <kaveri3149142@gmail.com>
2025-05-05 09:22:18 -0500

    Added toaster

--- src/layouts/landingPage.jsx
+++ src/layouts/landingPage.jsx
@@ -3,14 +3,38 @@ import HighlightedProductsCarousel from './HighlightedProductsCarousel';
 import SidebarFilters from './SidebarFilters';
 import ProductGrid from '../features/products/ProductGrid';
 import withLayout from '../HOC/withLayout';
+import { ToastContainer, toast } from 'react-toastify';
 
 const LandingPage = () => {
-    
+
   const [searchQuery, setSearchQuery] = useState('');
   const [selectedCategory, setSelectedCategory] = useState('');
+
+  const showToaster = (type) => {
+    if (type === "success") {
+      toast.success("Success message !!", {
+        position: 'bottom-center'
+      });
+
+    }
+    else if (type === "error") {
+      toast.error("Error message !!", {
+        position: 'bottom-center'
+      });
+    }
+  }
+
+
   return (
-     <>
+    <>
       <HighlightedProductsCarousel />
+
+      <div>
+        <button onClick={() => showToaster('success')}>Success</button>
+        <button onClick={() => showToaster('error')}>Error</button>
+        <ToastContainer />
+      </div>
+
       <div className="main-content">
         <div className="filters-and-grid">
           <SidebarFilters onFilterChange={setSelectedCategory} />
